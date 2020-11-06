e658c55273f4627908f2ae4d76c2c0556cc1ba1d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-11-06 23:57:49 +0700

    กำหนดเค้าโครงข้อมูลด้วย schema
    กำหนดเค้าโครงข้อมูลด้วย schema

---
+++ database.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const config = {
+  autoIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectionString = `mongodb+srv://admin:<>Boomgt123@cluster0.utr9m.mongodb.net/<dbname>?retryWrites=true&w=majority
+`;
+
+mongoose
+  .connect(connectionString, config)
+  .then(() => console.log("Connect to MongoDB..."))
+  .catch((err) => console.log("Cannot connect to MongoDB", err));
+
+// กำหนดเค้าโครงข้อมูลด้วย schema
+const studentSchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  class: String,
+  hobbies: [String],
+  isStudying: { type: Boolean, default: false },
+  score: { type: Number, default: 0 },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+const createStudent = async() => {
+  const student = Student({
+    id: "0523301122",
+    name: "Worrawank",
+    hobbies: ["Running", "Pingpong"],
+    class: '6/1',
+    score: 84,
+    isStudying: true,
+  });
+
+  const data = await student.save();
+  console.log(data);
+  
+}
+
+createStudent();
